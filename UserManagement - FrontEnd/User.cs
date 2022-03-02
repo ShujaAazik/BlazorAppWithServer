0c4afb5f0d4b3f1ac30e27fda430682e048e2036
Suja Aazik <Suja.Aazik@sureservegroup.co.uk>
2022-03-02 22:18:44 +0000

    Updated Web Layout

--- UserManagement - FrontEnd/User.cs
+++ UserManagement - FrontEnd/User.cs
@@ -1,13 +1,21 @@
-﻿namespace UserManagement___FrontEnd
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagement___FrontEnd
 {
     public class User
     {
         public int UserId { get; set; }
 
+        [Required]
+        [StringLength(25,ErrorMessage = "Name is too long.")]
         public string Name { get; set; }
 
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
 
+        [Required]
+        [StringLength(250, ErrorMessage = "Name is too long.")]
         public string Address { get; set; }
     }
 }
